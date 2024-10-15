1cbe5d94808a5e30d9950935c758a175ea53b586
rahul2103 <bluebash@hello.local>
2024-10-16 00:17:57 +0530

    remove vendor

--- app/controllers/spree/admin/print_invoice_settings_controller.rb
+++ app/controllers/spree/admin/print_invoice_settings_controller.rb
@@ -4,7 +4,7 @@ module Spree
       before_action :find_or_create_setting, only: [:index, :update]
 
       def update
-        @print_invoice_setting.update(print_invoice_setting_params)
+        settings = current_store.print_invoice_setting.update(print_invoice_setting_params)
 
         flash[:success] = Spree.t(:successfully_updated, resource: Spree.t(:settings, scope: :print_invoice))
         redirect_to admin_print_invoice_settings_path
@@ -13,18 +13,13 @@ module Spree
       private
 
       def find_or_create_setting
-        @print_invoice_setting = if current_spree_vendor.present? && current_spree_vendor.print_invoice_setting.present?
-                                    current_spree_vendor.print_invoice_setting
-        else
-                                    Spree::PrintInvoiceSetting.create!(store_id: current_store.id,
-                                                                       vendor_id: current_spree_vendor.try(:id))
+        unless @print_invoice_setting = current_store.print_invoice_setting
+          @print_invoice_setting = Spree::PrintInvoiceSetting.create!(store_id: current_store.id)
         end
       end
 
       def print_invoice_setting_params
-        params.require(:print_invoice_setting).permit(:next_number, :logo_path, :page_size, :page_layout, :footer_left, :footer_right,
-                                                      :return_message, :anomaly_message, :use_footer, :use_page_numbers, :logo_scale, :font_face,
-                                                      :font_size, :store_pdf, :storage_path, :store_id, :logo)
+        params.require(:print_invoice_setting).permit(:next_number, :logo_path, :page_size, :page_layout, :footer_left, :footer_right, :return_message, :anomaly_message, :use_footer, :use_page_numbers, :logo_scale, :font_face, :font_size, :store_pdf, :storage_path, :store_id )
       end
     end
   end
